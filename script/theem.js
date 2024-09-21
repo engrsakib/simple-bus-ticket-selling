38ffbebe0459c1f15e12103eb29302642cf6ae57
engrsakib <snazmussakib01@gmail.com>
2024-09-21 23:27:55 +0600

    crasol added

---
+++ script/theem.js
@@ -0,0 +1,4 @@
+document.getElementsByTagName('html')[0].setAttribute('data-theme', 'light');
+
+
+// https://web.programming-hero.com/web-10/video/reward-video-unit?reward=Milestone-5:-B9A5-Assignment-Solution-utnp 
